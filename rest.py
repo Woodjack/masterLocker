89170c0bad47bf2637dc9ef71ba214cf57f4fcc0
Justin Woodjack <jwoodjack@gmail.com>
2013-07-10 15:11:02 -0700

    post and get working

--- rest.py
+++ rest.py
@@ -3,25 +3,22 @@ import pymongo
 from bson.json_util import dumps
 from urlparse import urlparse, parse_qs  #url parsing for query
 
-mongohq_url = 'mongodb://pull:pull@dharma.mongohq.com:10014/app16815592'
+mongohq_url = 'mongodb://rest:service@dharma.mongohq.com:10014/app16815592'
 connection = pymongo.Connection(mongohq_url)
 db = connection.app16815592
 
 def getMONGO():
 	coll = db.locations
-	mongoResults = coll.find()
+	mongoResults = coll.find({},{'_id':0})
 	if mongoResults:
 	    results = dumps(mongoResults)
 	    return(results)
 	else:
 	    return('No Results')
 
-	    
 
-def pushLocation(urlQuery):
+
+def postLocation(newLocation):
 	coll = db.locations
-	data = parse_qs(urlparse(urlQuery).query)
-	jsonLocation = {}
-	
-	#coll.insert(jsonLocation)
-	return(data)
+	coll.insert(newLocation)
+	return("Successful mongodb upload bitches!!! ")
