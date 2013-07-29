ff411c4bdeca7f1960634eef4e99514bd5916515
Justin Woodjack <jwoodjack@gmail.com>
2013-07-28 17:18:46 -0700

    Updated for  localhost  mongodb  for dev

--- mongodbconf.py
+++ mongodbconf.py
@@ -1,5 +1,8 @@
 import pymongo
 
-mongohq_url = 'mongodb://rest:service@dharma.mongohq.com:10014/app16815592'
-connection = pymongo.Connection(mongohq_url)
+try:
+	connection = pymongo.Connection('localhost')
+except:
+	connection = pymongo.Connection('mongodb://rest:service@dharma.mongohq.com:10014/app16815592')
+
 db = connection.app16815592
