5fa79bd0965f9f0ff7c365f5978c779bd24d4368
Justin Woodjack <jwoodjack@gmail.com>
2013-08-01 03:12:27 -0700

    This thing is starting to be AWESOME!!

--- mapping.py
+++ mapping.py
@@ -2,24 +2,25 @@ from mongodb import db
 from bson.json_util import dumps
 from bson.json_util import loads
 
+
+## This function takes a pymongo .find()
+## result and makes it into one single polyline
 def makeLineFromPoints(pointsJSON):
-	
 	results = {}
 	pointsJSON = loads(pointsJSON)
 	temp = pointsJSON[0]
-	results['name'] = temp['name']
+	results['name'] = str(temp['name'])
 	results['type'] = 'polyline'
 	results['points'] = []
 	for i in pointsJSON:
-		results['points'].append(  [ i['loc']['x'], i['loc']['y']] )
-	print results
+		results['points'].append([  float(i['loc']['x']) , float(i['loc']['y']) ])
+	return results
 
 
-
-coll = db.events
-mongoResults = coll.find({'name':'Justin'},{'_id':0})[:5]
-if mongoResults:
-    results = dumps(mongoResults)
-    makeLineFromPoints(results)
-else:
-    print('No Results')
+def testing():
+	mongoResults = coll.find({'name':'Justin'},{'_id':0})[:5] #do a query for justin, only give back 5 results
+	if mongoResults:
+	    results = dumps(mongoResults) #put into a string
+	    print( makeLineFromPoints(results) ) #make it into a line
+	else:
+	    print('No Results')
