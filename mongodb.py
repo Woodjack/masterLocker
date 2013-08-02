6777a839fe2c151246bf741e2c46043410acd4f9
scrivy <scrivinator@gmail.com>
2013-08-02 00:07:42 -0700

    using local mongo as default

--- mongodb.py
+++ mongodb.py
@@ -1,9 +1,5 @@
 import pymongo
 
+connection = pymongo.Connection('localhost')
 
-try:
-	connection = pymongo.Connection('localhost')
-except:
-	connection = pymongo.Connection('mongodb://rest:service@dharma.mongohq.com:10014/app16815592')
-
-db = connection.app16815592
+db = connection.wheresjack
