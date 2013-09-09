aa5e235541a01cb7c7140636b04331ced86b1a72
Justin Woodjack <jwoodjack@gmail.com>
2013-09-09 15:41:03 -0700

    more info

--- server.py
+++ server.py
@@ -49,7 +49,8 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         livedata['data'] = rest.getLive()
         self.write_message(livedata)
         self.write_message("connection acknowledged")
-        print self.clients
+        print self.clients.count()
+        print 'type:  ' + str(type(self.clients))
 
     def on_message(self, message):
         clientdata = json.loads(message)
