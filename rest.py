73eaf90ea19e469798cb5237c8c0bfea23bd487d
scrivy <scrivinator@gmail.com>
2013-09-09 04:50:05 -0700

    removed ajax code

--- rest.py
+++ rest.py
@@ -6,25 +6,6 @@ from bson.json_util import dumps
 from bson.json_util import loads
 import ast
 
-def getMONGO():
-	coll = db.events
-	mongoResults = coll.find({},{'_id':0})
-	if mongoResults:
-	    results = dumps(mongoResults)
-	    return(results)
-	else:
-	    return('No Results')
-
-def postNewUser(data):
-	coll = db.users
-	coll.insert(data)
-
-def updateUserName(data):
-	coll = db.users
-	coll.update(data)
-
-
-
 def getLive():
 	data=[]
 	eventsDB = db.events
@@ -49,30 +30,6 @@ def getLive():
 	    return( dumps(data) )
 	else:
 	    return("getLive: No Results" )
-
-
-def getLiveWithoutMe(cookieID):
-	data = getLive()
-	data = ast.literal_eval(data) #This turns it into a list, if you want it as a dict, put a [0] in the bound
-	print(data)
-	print(type(data)) #type is a list
-
-
-
-
-
-def getCurrentMONGO():
-	data=[]
-	coll = db.events	
-	query = coll.distinct('name')
-	for person in query:
-		personinfo = coll.find({'name': person},{'_id': 0}).sort('date',1).limit(1)
-		getResults = dumps(personinfo)
-		data.append( ast.literal_eval(getResults)[0] )
-	if data:
-	    return( dumps(data) )
-	else:
-	    return( 'No Results' )
 
 def postLocation(newLocation):
 	coll = db.events
--- server.py
+++ server.py
@@ -20,10 +20,7 @@ define("port", default=port, help="run on the given port", type=int) #port optio
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/rest/get/current", getCurrentRequestHandler),
             (r"/admin/rest/dumpallpoints", dumpallpointsHandler),
-            (r"/cookie", cookieRequestHandler),
-            (r"/updatename", updateNameRequestHandler),
             (r"/ws", WSHandler),
         	(r"/(.+)", tornado.web.StaticFileHandler, {"path": "static"}),
         	(r"/", indexhtmlhandler)
@@ -33,40 +30,6 @@ class Application(tornado.web.Application):
 class indexhtmlhandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render("static/index.html")
-
-class getCurrentRequestHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write( rest.getCurrentMONGO() )
-
-class cookieRequestHandler(tornado.web.RequestHandler):
-    def get(self):
-        if not self.get_cookie( 'id' ):
-            data = {}
-            data['cookie'] = cookies.bakeCookie()
-            data['date'] = datetime.datetime.utcnow()
-            data['name'] = self.get_argument('name')
-            print('cookie baked!!  Username: ' + str(data['name'] ))
-            self.set_cookie( 'id' , str(data['cookie']) ,expires_days=14 )
-            self.set_cookie( 'name' , str(data['name']) ,expires_days=14 )
-            rest.postNewUser(data)
-            self.write('New Cookies generated successfully')
-        else:
-            self.write('Cookie already there')
-
-class updateNameRequestHandler(tornado.web.RequestHandler):
-    def get(self):
-        try:
-            id_cookieValue = cookies.bakeCookie()
-            data = {}
-            data['cookie'] = id_cookieValue
-            data['date'] = datetime.datetime.utcnow()
-            data['name'] = self.get_argument('name')
-            self.write('cookie updated!!')
-            self.set_cookie( 'id' , str(id_cookieValue) ,expires_days=14 )
-            self.set_cookie( 'name' , str(data['name']) ,expires_days=14 )
-            rest.updateUserName(data)
-        except:
-            self.write('no cookie found')
 
 class dumpallpointsHandler(tornado.web.RequestHandler):
     def get(self):
@@ -82,8 +45,6 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         self.write_message("connection acknowledged")
 
     def on_message(self, message):
-        print self.clients
-
         clientdata = json.loads(message)
 
         print 'clientdata', repr(clientdata)
