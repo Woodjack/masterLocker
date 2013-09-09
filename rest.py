04393b999cf4113248bbc0cc940fba784fa06e7c
Justin Woodjack <jwoodjack@gmail.com>
2013-09-09 14:48:28 -0700

    working with less prints on getLive()

--- rest.py
+++ rest.py
@@ -16,8 +16,6 @@ def getLive():
 		personinfo = eventsDB.find({'cookie': cookie, "date": { "$gte": date } },{'_id': 0,'date':0,'cookie':0}).sort('date',1).limit(1)
 		getResults = dumps(personinfo)
 
-		print repr(getResults)
-
 		if getResults != "[]":
 			try:
 				results = ast.literal_eval(getResults)[0] #this translates getResults as a list, then grabs the first (and only) element
