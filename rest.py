248f56d609f1c1529ccc75ba78918814ba673425
scrivy <scrivinator@gmail.com>
2013-09-17 11:30:22 -0700

    limit admin points

--- rest.py
+++ rest.py
@@ -88,7 +88,7 @@ def getMessageMarkers():
 def dumpallpoints():
 	data=[]
 	coll = db.events
-	for point in db.events.find():
+	for point in db.events.find().limit(1000).sort('date', 1);
 		data.append( point )
 	if data:
 		return( dumps(data) )
